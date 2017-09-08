fb6fea98af694d3440fd4eb3948c9189ddd5d907
imrkv <rk.vithlani@live.com>
2017-09-08 11:17:18 +0530

    new feature completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function aNewBigFeature(){
 	console.log('Lots of code supposed to be here');
 	console.log('halfway done with new feature');
+	console.log('Finally done with new feature');
 };
 function something(){
 	console.log('I\'m Something');
