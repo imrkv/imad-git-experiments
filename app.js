542122c589076c86e1fb24a02a3aecff85aae803
imrkv <rk.vithlani@live.com>
2017-09-08 10:57:33 +0530

    half way done with new feature.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+function aNewBigFeature(){
+	console.log('Lots of code supposed to be here');
+	console.log('halfway done with new feature');
+};
 function something(){
-	console.log('I/'m Something');
+	console.log('I\'m Something');
 }
 console.log('hello');
 something(); 
+aNewBigFeature();
